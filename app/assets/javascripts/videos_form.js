d1836078c15d1bb6e1ff938025c6cc35577ba25a
kumartushar <kumartushar1111@gmail.com>
2017-07-13 20:06:04 +0530

    Upload file using FormData and Ajax: 13 July 2017.

--- app/assets/javascripts/videos_form.js
+++ app/assets/javascripts/videos_form.js
@@ -47,11 +47,47 @@ $(document).ready(function(e) {
 
   $("#mt-upload-meta").find("*").prop("disabled", true);
 
-  $("#target_video").change(function(e){
-    $("#mt-file-upload-title").text("File Selected: " + this.value);
-    $("#mt-file-title-box").show();
-    $("#mt-upload-meta").find("*").prop("disabled", false);
-    $("#mt-upload-submit").prop("disabled", false);
+  // $("#target_video").change(function(e){
+    // $("#mt-file-upload-title").text("File Selected: " + this.value);
+    // $("#mt-file-title-box").show();
+    // $("#mt-upload-meta").find("*").prop("disabled", false);
+    // $("#mt-upload-submit").prop("disabled", false);
+  //   alert("hi");
+  // });
+  $(document).on("change", "#target_video", function(e) {
+    var formData = new FormData();
+    formData.append("target_file", e.target.files[0]);
+
+    //------------------AJAX with JQuery
+    $.ajax({
+      url: '/upload_video',
+      type: 'POST',
+      data: formData,
+      cache: false,
+      dataType: 'script',
+      processData: false, // Don't process the files,  Because jQuery will convert the files arrays into strings and the server can't pick it up.
+      contentType: false, // Set content type to false as jQuery will tell the server its a query string request, if true it doesnt send the files
+      success: function(data, textStatus, jqXHR) {
+        alert("file uploaded successfully");
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        alert("Error uploading file");
+      }
+    });
+
+    //------------------ AJAX with pure JavaScript
+    // var token = $( 'meta[name="csrf-token"]' ).attr( 'content' );
+    // var xhr = new XMLHttpRequest();
+    // xhr.open('POST', "/upload_video", true);
+    // xhr.onload = function() {
+    //   if (xhr.status === 200) {
+    //     alert("file uploaded successfully");
+    //   } else {
+    //     alert("Error File Uploading");
+    //   }
+    // };
+    // xhr.setRequestHeader( 'X-CSRF-Token', token );
+    // xhr.send(formData);
   });
 
   $(document).on("click", "#mt-add-tag-btn", function(e) {
