72ccc2775e203fe456aa74ef57caba8ca25604bc
kumartushar <kumartushar1111@gmail.com>
2017-07-16 20:30:21 +0530

    UI modifications to video upload functionality: 16 July 2017.

--- app/assets/javascripts/videos_form.js
+++ app/assets/javascripts/videos_form.js
@@ -10,12 +10,10 @@ $(document).ready(function(e) {
   });
   
   $('.cloudinary-fileupload').bind('cloudinarydone', function(e, data) {
-    // $('.preview').html(
-    // $.cloudinary.image(data.result.public_id,
-    //   { format: data.result.format, version: data.result.version,
-    //     crop: 'fill', width: 150, height: 100 })
-    // );
-    // $('.image_public_id').val(data.result.public_id);
+    $('.mt-video-thumbnail').html(
+      $.cloudinary.image(data.result.public_id, { format: "png", crop: 'fill', width: 150, height: 100 })
+    );
+    $('.image_public_id').val(data.result.public_id);
     $('.progress-bar').removeClass('progress-bar-info').addClass("progress-bar-success");
     $("#mt-upload-progress-text").text("Click 'Upload' to make your video live.");
     return true;
